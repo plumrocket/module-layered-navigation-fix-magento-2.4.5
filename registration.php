47bd61e2f75f506711c0327841e4d2a95c92ee09
Dmytro Melnychuk <dmytro.melnychuk@plumrocket.com>
2022-10-05 11:40:08 +0300

    first commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     Plumrocket_ProductFilterFixMagento245
+ * @copyright   Copyright (c) 2022 Plumrocket Inc. (https://plumrocket.com)
+ * @license     https://plumrocket.com/license   End-user License Agreement
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Plumrocket_ProductFilterFixMagento245', __DIR__);
